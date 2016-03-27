16be11c5a25fdcae8eb9cec1965cdfad32bd3eee
madhuri <rmadhuri2015@gmail.com>
2016-03-27 17:19:30 -0500

    refactor rename renew

---
+++ src/main/java/digital/places/role/BeforeFindAllRoles.java
@@ -0,0 +1,15 @@
+package digital.places.role;
+
+import java.lang.reflect.Method;
+
+import org.springframework.aop.MethodBeforeAdvice;
+
+public class BeforeFindAllRoles implements MethodBeforeAdvice
+{
+	@Override
+	public void before(Method method, Object[] args, Object target)
+		throws Throwable 
+	{
+        System.out.println(method.getName());
+	}
+}
