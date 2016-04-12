4035e641388010870aafe6328a93b4edca48e298
madhuri <rmadhuri2015@gmail.com>
2016-04-12 12:16:11 -0500

    data organization

--- src/main/java/digital/places/root/AuthObject.java
+++ src/main/java/digital/places/root/AuthObject.java
@@ -8,31 +8,8 @@ import java.util.Map;
 
 import org.springframework.util.StringUtils;
 
-public abstract class AuthObject
+public abstract class AuthObject implements Links
 {
-    public static final String PAGE403 = "403";
-    public static final String PAGE403URL = "/403";
-
-    public static final String LOGINPAGE = "login";
-    public static final String LOGINPAGEURL = "/login";
-
-    public static final String SEARCHPAGEURL ="/search";
-    public static final String SEARCHPAGE ="search";
-
-    public static final String ADDPAGE = "add";
-    public static final String ADDPAGEURL = "/add";
-
-    public static final String UPDATEPAGE = "update";
-    public static final String UPDATEPAGEURL = "/update";
-
-    public static final String CONFIRMPAGE = "confirm";
-
-    public static final String ADDPAGERESTURL = "/add/{uname}";
-
-    public static final String VIEWALLPAGE = "viewall";
-    public static final String VIEWALLPAGEURL = "/viewall";
-
-    
     public static final LinkedHashMap<String,String> UYYYY = new LinkedHashMap<String,String>();
 
     static
---
+++ src/main/java/digital/places/root/Links.java
@@ -0,0 +1,30 @@
+package digital.places.root;
+
+public interface Links 
+{
+
+    public static final String PAGE403 = "403";
+    public static final String PAGE403URL = "/403";
+
+    public static final String LOGINPAGE = "login";
+    public static final String LOGINPAGEURL = "/login";
+
+    public static final String SEARCHPAGEURL ="/search";
+    public static final String SEARCHPAGE ="search";
+
+    public static final String ADDPAGE = "add";
+    public static final String ADDPAGEURL = "/add";
+
+    public static final String UPDATEPAGE = "update";
+    public static final String UPDATEPAGEURL = "/update";
+
+    public static final String CONFIRMPAGE = "confirm";
+
+    public static final String ADDPAGERESTURL = "/add/{uname}";
+
+    public static final String VIEWALLPAGE = "viewall";
+    public static final String VIEWALLPAGEURL = "/viewall";
+
+    
+
+}
