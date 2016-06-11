c519269515a3ad3456eb81beef4273fa7df2de67
madhuri <rmadhuri2015@gmail.com>
2016-06-11 14:44:58 -0500

    wired in selenium htmlunit testing and mockito testing

---
+++ src/test/java/digital/places/root/AppMailerTest.java
@@ -0,0 +1,41 @@
+package digital.places.root;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AppMailerTest 
+{
+	@Mock
+	private MailSender mailSender;
+
+	@Mock
+	private SimpleMailMessage preConfiguredMessage;
+
+	@InjectMocks
+	private AppMailer appMailer;
+
+	@BeforeMethod (groups= "mockito")
+	public void mockSend()
+	{
+		MockitoAnnotations.initMocks(this);
+		Mockito.doNothing().when(mailSender).send(preConfiguredMessage);
+	}
+	
+	@Test (groups= "mockito")
+	public void sendMailTest()
+	{
+		appMailer.sendMail("rmadhrui2014@gas.com", "Test", "Testtest");	
+	}
+
+	@Test (groups= "mockito")
+	public void sendPreConfiguredMailTest()
+	{
+		appMailer.sendPreConfiguredMail("PreTesttest");	
+	}
+}
