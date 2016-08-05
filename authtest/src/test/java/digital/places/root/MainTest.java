601f21cc676f12def81e1dc93018212053664748
madhuri <rmadhuri2015@gmail.com>
2016-08-05 00:03:35 -0500

    updated exception handling

--- authtest/src/test/java/digital/places/root/MainTest.java
+++ authtest/src/test/java/digital/places/root/MainTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -14,6 +15,7 @@ public class MainTest
 	private final WebDriver wdriver = new HtmlUnitDriver(true);
 
 	private final String uri = "http://localhost:8080/auth";
+	private final String uri2 = "http://localhost:8080/somenonexistenturi";
 	
 	public ResponseEntity<String> testStatusCode(final String uri)
 	{
@@ -31,17 +33,23 @@ public class MainTest
 	public String testMetaDescription(final String uri)
 	{
 		wdriver.get(uri);
-		return wdriver.findElement(By.xpath("//meta[@name='description']@content")).getText();
+		return wdriver.findElement(By.xpath("//meta[@name='description']")).getAttribute("content");
 	}
 
 	@Test
 	public void testPageLoad()
 	{
 		String title = testTitle(uri);
-		Assert.assertEquals(title, "Auth - Login Page");
+		Assert.assertEquals(title, "Auth - Sitemap");
 		String desc = testMetaDescription(uri);
+		Assert.assertEquals(desc, "Sitemap page");
 		ResponseEntity<String> response = testStatusCode(uri);
-		Assert.assertEquals(response.getStatusCode(),HttpStatus.NOT_FOUND);
+		Assert.assertEquals(response.getStatusCode(),HttpStatus.OK);
 	}
-	
+
+	@Test(expectedExceptions = HttpClientErrorException.class,expectedExceptionsMessageRegExp = "404 Not Found")
+	public void testNonExistentURI()
+	{
+		ResponseEntity<String> response = testStatusCode(uri2);
+	}
 }
